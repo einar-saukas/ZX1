4e8a229dfb2c8e9a2871005f152d45b9deb7d3b7
einar-saukas <einar@summa.com.br>
2021-09-28 15:26:46 -0300

    improved compressor

--- src/compress.c
+++ src/compress.c
@@ -64,7 +64,7 @@ void write_interlaced_elias_gamma(int value) {
     for (i = 2; i <= value; i <<= 1)
         ;
     i >>= 1;
-    while ((i >>= 1) > 0) {
+    while (i >>= 1) {
         write_bit(1);
         write_bit(value & i);
     }
@@ -72,10 +72,11 @@ void write_interlaced_elias_gamma(int value) {
 }
 
 unsigned char *compress(BLOCK *optimal, unsigned char *input_data, int input_size, int skip, int backwards_mode, int *output_size, int *delta) {
-    BLOCK *next;
     BLOCK *prev;
+    BLOCK *next;
     int last_offset = INITIAL_OFFSET;
     int first = TRUE;
+    int length;
     int i;
 
     /* calculate and allocate output buffer */
@@ -91,19 +92,21 @@ unsigned char *compress(BLOCK *optimal, unsigned char *input_data, int input_siz
     *delta = 0;
 
     /* un-reverse optimal sequence */
-    next = NULL;
+    prev = NULL;
     while (optimal) {
-        prev = optimal->chain;
-        optimal->chain = next;
-        next = optimal;
-        optimal = prev;
+        next = optimal->chain;
+        optimal->chain = prev;
+        prev = optimal;
+        optimal = next;
     }
 
     input_index = skip;
     output_index = 0;
     bit_mask = 0;
 
-    for (optimal = next->chain; optimal; optimal = optimal->chain) {
+    for (optimal = prev->chain; optimal; prev=optimal, optimal = optimal->chain) {
+        length = optimal->index-prev->index;
+
         if (!optimal->offset) {
             /* copy literals indicator */
             if (first)
@@ -112,10 +115,10 @@ unsigned char *compress(BLOCK *optimal, unsigned char *input_data, int input_siz
                 write_bit(0);
 
             /* copy literals length */
-            write_interlaced_elias_gamma(optimal->length);
+            write_interlaced_elias_gamma(length);
 
             /* copy literals values */
-            for (i = 0; i < optimal->length; i++) {
+            for (i = 0; i < length; i++) {
                 write_byte(input_data[input_index]);
                 read_bytes(1, delta);
             }
@@ -124,8 +127,8 @@ unsigned char *compress(BLOCK *optimal, unsigned char *input_data, int input_siz
             write_bit(0);
 
             /* copy from last offset length */
-            write_interlaced_elias_gamma(optimal->length);
-            read_bytes(optimal->length, delta);
+            write_interlaced_elias_gamma(length);
+            read_bytes(length, delta);
         } else {
             /* copy from new offset indicator */
             write_bit(1);
@@ -148,8 +151,8 @@ unsigned char *compress(BLOCK *optimal, unsigned char *input_data, int input_siz
             }
 
             /* copy from new offset length */
-            write_interlaced_elias_gamma(optimal->length-1);
-            read_bytes(optimal->length, delta);
+            write_interlaced_elias_gamma(length-1);
+            read_bytes(length, delta);
 
             last_offset = optimal->offset;
         }
--- src/optimize.c
+++ src/optimize.c
@@ -30,18 +30,14 @@
 
 #define MAX_SCALE 50
 
-#define minimum(a,b) (a < b ? a : b)
-
 int offset_ceiling(int index, int offset_limit) {
     return index > offset_limit ? offset_limit : index < INITIAL_OFFSET ? INITIAL_OFFSET : index;
 }
 
 int elias_gamma_bits(int value) {
     int bits = 1;
-    while (value > 1) {
+    while (value >>= 1)
         bits += 2;
-        value >>= 1;
-    }
     return bits;
 }
 
@@ -73,7 +69,7 @@ BLOCK* optimize(unsigned char *input_data, int input_size, int skip, int offset_
     best_length[2] = 2;
 
     /* start with fake block */
-    assign(&(last_match[INITIAL_OFFSET]), allocate(-1, skip-1, INITIAL_OFFSET, 0, NULL));
+    assign(&last_match[INITIAL_OFFSET], allocate(-1, skip-1, INITIAL_OFFSET, NULL));
 
     printf("[");
 
@@ -87,9 +83,9 @@ BLOCK* optimize(unsigned char *input_data, int input_size, int skip, int offset_
                 if (last_literal[offset]) {
                     length = index-last_literal[offset]->index;
                     bits = last_literal[offset]->bits + 1 + elias_gamma_bits(length);
-                    assign(&(last_match[offset]), allocate(bits, index, offset, length, last_literal[offset]));
+                    assign(&last_match[offset], allocate(bits, index, offset, last_literal[offset]));
                     if (!optimal[index] || optimal[index]->bits > bits)
-                        assign(&(optimal[index]), last_match[offset]);
+                        assign(&optimal[index], last_match[offset]);
                 }
                 /* copy from new offset */
                 if (++match_length[offset] > 1) {
@@ -109,9 +105,9 @@ BLOCK* optimize(unsigned char *input_data, int input_size, int skip, int offset_
                     length = best_length[match_length[offset]];
                     bits = optimal[index-length]->bits + 1 + (offset > 128 ? 16 : 8) + elias_gamma_bits(length-1);
                     if (!last_match[offset] || last_match[offset]->index != index || last_match[offset]->bits > bits) {
-                        assign(&last_match[offset], allocate(bits, index, offset, length, optimal[index-length]));
+                        assign(&last_match[offset], allocate(bits, index, offset, optimal[index-length]));
                         if (!optimal[index] || optimal[index]->bits > bits)
-                            assign(&(optimal[index]), last_match[offset]);
+                            assign(&optimal[index], last_match[offset]);
                     }
                 }
             } else {
@@ -120,9 +116,9 @@ BLOCK* optimize(unsigned char *input_data, int input_size, int skip, int offset_
                 if (last_match[offset]) {
                     length = index-last_match[offset]->index;
                     bits = last_match[offset]->bits + 1 + elias_gamma_bits(length) + length*8;
-                    assign(&(last_literal[offset]), allocate(bits, index, 0, length, last_match[offset]));
+                    assign(&last_literal[offset], allocate(bits, index, 0, last_match[offset]));
                     if (!optimal[index] || optimal[index]->bits > bits)
-                        assign(&(optimal[index]), last_literal[offset]);
+                        assign(&optimal[index], last_literal[offset]);
                 }
             }
         }
